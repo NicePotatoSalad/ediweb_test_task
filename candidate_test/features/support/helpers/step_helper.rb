6077ac11967584b390475ebe1a711298aa426e8e
NicePotatoSalad <programming.here2006@gmail.com>
2025-06-29 14:09:57 +0200

    commented on existing code

--- candidate_test/features/support/helpers/step_helper.rb
+++ candidate_test/features/support/helpers/step_helper.rb
@@ -9,16 +9,19 @@ require 'fileutils' # We need this for FileUtils operations
 def find_user_id(users_information:, user_login:)
   users_id = []
   users_information.each do |user|
-    next unless user['login'] == user_login
+    # .try(:[], 'login') is a safe way to access 'login' key, avoids error if 'user' is nil or doesn't have 'login'
+    next unless user.try(:[], 'login') == user_login
 
-    users_id << user['id']
+    users_id << user['id'] # Add user's ID to the list
   end
-  users_id.uniq!
+  users_id.uniq! # Remove duplicate IDs
 
-  if users_id.size != 1
+  if users_id.empty?
+    raise "Пользователь с логином #{user_login} не найден." # If no users found, raise error
+  elsif users_id.size != 1
+    # If more than one user with the same login, this is an issue (login should be unique)
     raise "Логин пользователя неуникален! Найдено пользователей с аналогичным логином: #{users_id.size}, id: #{users_id.inspect}"
   end
 
-  users_id.first
+  users_id.first # Return the single unique user ID
 end
-
